6a9e17dd502e47d457bfaa19930d0dd2995db449
Bruno Ruas <bruno.ruas2@gmail.com>
2021-12-07 08:03:47 -0400

    minor updates

--- Microeconomia/Microeconomics - Hal Varian/models/cap25.3-demanda_ces_e_monopolio.py
+++ Microeconomia/Microeconomics - Hal Varian/models/cap25.3-demanda_ces_e_monopolio.py
@@ -1,6 +1,6 @@
 ### Universidade do Estado do Amazonas - UEA (Microeconomia II)
-## Modelo de Otimização da Receita do Monopólio com uma Demanda linear
-# Varian página 632
+## Modelo de Otimização da Receita do Monopólio com uma Demanda com Elasticidade Constante
+# Varian página 634
 
 # Modulos usados
 import pandas as pd
@@ -8,40 +8,39 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+from math import inf
+
 pd.options.mode.chained_assignment = None  # default='warn'
 
 plt.style.use('dark_background')
 
 # Sistema de Equações:
-# Curva de Demanda Inversa Linear -> p(y) = a - by
+# Curva de Demanda Inversa com Elasticidade Constante -> p(y) = (k/y)^(1/alpha)
 # Função Receita -> r(y) = p(y)y = ay - by^2
 # Função Receita Marginal -> RM(y) = a - 2by
 # Função Custo Total -> c(y) = CF + 5*CV*y + (CV*y^3)/3
 # Função Custo Marginal -> CMa(y) = CV
 
 # Constantes usadas nas equações:
-a = 100
-b = 5
+alpha = 5
+k = 100000000
 CF = 150 # custo fixo
 CV = 1 # custo variavel
 
-
 # Modelo
-y = np.linspace(0,100,1000)
+y = np.linspace(0.01,100,1000)
 
-dem_lin = a - b * y
-receita = a * y - b * (y ** 2)
-rec_marg = a - 2*b*y
+dem_ces = (k/y)**(1/alpha)
+receita = dem_ces*y
+rec_marg = ((alpha - 1)/(alpha))*(y**(-1/alpha))*(k**(1/alpha))
 custo_total = CF + 5*CV*y + (CV*y**3)/3
 custo_medio = custo_total / y
 custo_marginal = 5*CV + CV*y**2
 lucro = receita - custo_total
-phi = 1 / (1 - abs(y/dem_lin) * b)
-oferta = phi * custo_marginal
 
 dataframe = pd.DataFrame()
 dataframe['quant'] = y
-dataframe['dem_lin'] = dem_lin
+dataframe['dem_ces'] = dem_ces
 dataframe['receita'] = receita
 dataframe['rec_marg'] = rec_marg
 dataframe['custo_total'] = custo_total
@@ -49,8 +48,7 @@ dataframe['custo_medio'] = custo_medio
 dataframe['custo_marginal'] = custo_marginal
 dataframe['lucro'] = lucro
 
-dataframe['oferta'] = oferta
-dataframe['oferta'] = dataframe['oferta'].map(lambda s: None if s <= 0 else s)
+dataframe.to_excel('teste.xlsx')
 
 # Gerando o gráfico
 fig = plt.figure(dpi=120)
@@ -61,21 +59,20 @@ plt.subplot(2,1,1) # rows, columns, panel number
 
 plt.title('cap 25.2 - Demanda Linear e Monopólio')
 
-plt.plot(dataframe['quant'], dataframe['dem_lin'],'-',color='red', label='Demanda')
+plt.plot(dataframe['quant'], dataframe['dem_ces'],'-',color='red', label='Demanda')
 plt.plot(dataframe['quant'], dataframe['rec_marg'],'-',color='blue', label='Receita Marginal')
 plt.plot(dataframe['quant'], dataframe['custo_medio'],'-',color='green', label='Custo Medio')
 plt.plot(dataframe['quant'], dataframe['custo_marginal'],'-',color='white', label='CMa')
-#plt.plot(dataframe['quant'], dataframe['oferta'],'-',color='yellow', label='Oferta Monopolista')
 plt.legend(loc='upper right')
-plt.ylim(0,max(dataframe['dem_lin']))
-plt.xlim(0,a/b)
+plt.ylim(0,max(dataframe['dem_ces'].iloc[1:10]))
+plt.xlim(0,max(dataframe['dem_ces'].iloc[1:10]))
 
 plt.subplot(2,1,2) # rows, columns, panel number
 plt.plot(dataframe['quant'], dataframe['lucro'],'--',color='white', label='Lucro')
 plt.plot(dataframe['quant'], dataframe['custo_total'],'--',color='blue', label='Custo Total')
 plt.plot(dataframe['quant'], dataframe['receita'],'--',color='red', label='Receita')
 plt.legend(loc='upper right')
-plt.ylim(0,max(dataframe['receita']))
-plt.xlim(0,a/b)
+plt.ylim(0,max(dataframe['receita'].iloc[1:dataframe.shape[0]]))
+plt.xlim(0,max(dataframe['dem_ces'].iloc[1:10]))
 
 plt.show()
