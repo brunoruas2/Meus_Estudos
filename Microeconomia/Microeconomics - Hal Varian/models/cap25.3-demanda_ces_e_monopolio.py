b151307b9ab5e6d0062caaf683530102cc706495
Bruno Ruas <bruno.ruas2@gmail.com>
2021-12-08 14:51:47 -0400

    Update cap25.3-demanda_ces_e_monopolio.py
    começando de novo

--- Microeconomia/Microeconomics - Hal Varian/models/cap25.3-demanda_ces_e_monopolio.py
+++ Microeconomia/Microeconomics - Hal Varian/models/cap25.3-demanda_ces_e_monopolio.py
@@ -1,6 +1,6 @@
 ### Universidade do Estado do Amazonas - UEA (Microeconomia II)
-## Modelo de Otimização da Receita do Monopólio com uma Demanda com Elasticidade Constante
-# Varian página 634
+## Modelo de Otimização da Receita do Monopólio com uma Demanda linear
+# Varian página 632
 
 # Modulos usados
 import pandas as pd
@@ -8,51 +8,59 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-from math import inf
+# funcoes usadas
+def bhaskara(a,b,c):
+  r1 = (-b + (b**2 - 4 * a * c)**0.5)/(2 * a)
+  r2 = (-b - (b**2 - 4 * a * c)**0.5)/(2 * a)
+
+  return [r1,r2]
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
 plt.style.use('dark_background')
 
 # Sistema de Equações:
-# Curva de Demanda Inversa com Elasticidade Constante -> p(y) = k * y^alpha
-# Função Receita -> r(y) = p(y)y = y^(1+alpha) * k
-# Função Receita Marginal -> RM(y) = (1 + alpha) * y^alpha * k
+# Curva de Demanda Inversa Linear -> p(y) = a - by
+# Função Receita -> r(y) = p(y)y = ay - by^2
+# Função Receita Marginal -> RM(y) = a - 2by
 # Função Custo Total -> c(y) = CF + 5*CV*y + (CV*y^3)/3
-# Função Custo Marginal -> CMa(y) = CV
+# Função Custo Marginal -> CMa(y) = 5*CV + CV^2
 
 # Constantes usadas nas equações:
-alpha = -1.2 # elasticidade
-k = 10
-CF = 1 # custo fixo
-CV = 1 # custo variavel
+a = 100
+b = 5
+CF = 100 # custo fixo
+CV = 2 # custo variavel
 
-pn = 10
-p0 = (pn/k)**(1/alpha)
 
 # Modelo
-y = np.linspace(0,pn,1000)
+y_otimo = max(bhaskara(CV,2*b,-a+5*CV))
+y = np.linspace(0,a/b,1000)
 
-dem_ces = k * (y**alpha)
-receita = dem_ces*y
-rec_marg = (1 + alpha) * (y**alpha) * k
+dem_lin = a - b * y
+receita = a * y - b * (y ** 2)
+rec_marg = a - 2*b*y
 custo_total = CF + 5*CV*y + (CV*y**3)/3
 custo_medio = custo_total / y
 custo_marginal = 5*CV + CV*y**2
 lucro = receita - custo_total
 
-phi = 1 / (1 - abs(alpha))
-oferta = phi * custo_marginal
+phi = 1 / (1 - abs(-b * (y/(a-b*y))))
+oferta = phi * (5*CV + CV*y_otimo**2) # markup de lerner
 
+# Dataframe
 dataframe = pd.DataFrame()
 dataframe['quant'] = y
-dataframe['dem_ces'] = dem_ces
+dataframe['dem_lin'] = dem_lin
 dataframe['receita'] = receita
 dataframe['rec_marg'] = rec_marg
 dataframe['custo_total'] = custo_total
 dataframe['custo_medio'] = custo_medio
 dataframe['custo_marginal'] = custo_marginal
 dataframe['lucro'] = lucro
+
+dataframe['oferta'] = oferta
+dataframe['oferta'] = dataframe['oferta'].map(lambda s: None if s <= 0 else s)
 
 # Gerando o gráfico
 fig = plt.figure(dpi=120)
@@ -63,13 +71,13 @@ plt.subplot(2,1,1) # rows, columns, panel number
 
 plt.title('cap 25.2 - Demanda Linear e Monopólio')
 
-plt.plot(dataframe['quant'], dataframe['dem_ces'],'-',color='red', label='Demanda')
-#plt.plot(dataframe['quant'], dataframe['rec_marg'],'-',color='blue', label='Receita Marginal')
-#plt.plot(dataframe['quant'], dataframe['custo_medio'],'-',color='green', label='Custo Medio')
+plt.plot(dataframe['quant'], dataframe['dem_lin'],'-',color='red', label='Demanda')
+plt.plot(dataframe['quant'], dataframe['rec_marg'],'-',color='blue', label='Receita Marginal')
+plt.plot(dataframe['quant'], dataframe['custo_medio'],'-',color='green', label='Custo Medio')
 plt.plot(dataframe['quant'], dataframe['custo_marginal'],'-',color='white', label='CMa')
-#plt.plot(dataframe['quant'], dataframe['oferta'],'-',color='yellow', label='Oferta Monopolista') # oferta do monopolista
+plt.plot(dataframe['quant'], dataframe['oferta'],'-',color='yellow', label='Oferta Monopolista') # oferta do monopolista
 plt.legend(loc='upper right')
-plt.ylim(0,max(dataframe['dem_ces']))
+plt.ylim(0,max(dataframe['dem_lin']))
 plt.xlim(0,max(y))
 
 plt.subplot(2,1,2) # rows, columns, panel number
@@ -77,7 +85,7 @@ plt.plot(dataframe['quant'], dataframe['lucro'],'--',color='white', label='Lucro
 plt.plot(dataframe['quant'], dataframe['custo_total'],'--',color='blue', label='Custo Total')
 plt.plot(dataframe['quant'], dataframe['receita'],'--',color='red', label='Receita')
 plt.legend(loc='upper right')
-plt.ylim(0,max(dataframe['receita'].iloc[1:dataframe.shape[0]]))
+plt.ylim(0,max(dataframe['receita']))
 plt.xlim(0,max(y))
 
 plt.show()
