dc3d12c02db5f3d054030415e9fbb350020003e0
Bruno Ruas <bruno.ruas2@gmail.com>
2022-01-05 17:43:00 -0400

    cap 30.5 (done)

---
+++ Microeconomia/Microeconomics - Hal Varian/models/scripts/varian_30.5_coexistencia.py
@@ -0,0 +1,117 @@
+# Modulos usados
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+import tabloo # to see the df
+
+from mpl_toolkits import mplot3d
+
+pd.options.mode.chained_assignment = None  # default='warn'
+
+plt.style.use('dark_background')
+
+#####################################################################################################
+# Funções usadas no modelo
+#####################################################################################################
+# curvas de melhor resposta
+def melhor_a(pb_left):
+	pb_right = 1 - pb_left
+
+	banco = pd.DataFrame()
+
+	i = 0
+	while i < 1.01:
+		pa_up = i
+		pa_down = abs(round(1 - pa_up,2))
+		coord = [-2,0,4,2]
+		result = (coord[0]*pa_up*pb_left) + (coord[1]*pa_down*pb_left) + (coord[2]*pa_up*pb_right) + (coord[3]*pa_down*pb_right)
+		banco = banco.append([[pa_up,pa_down,pb_left,pb_right,result]])
+		i = i + 0.01
+	
+	banco.columns = ['pa_up','pa_down','pb_left','pb_right','result']
+
+	if banco[banco['result'] == max(banco['result'])]['pa_up'].shape[0] != 1:
+		return max(banco[banco['result'] == max(banco['result'])]['pa_up'])
+	else:
+		return max(banco[banco['result'] == max(banco['result'])]['pa_up'])
+
+def melhor_b(pa_up):
+	pa_down = 1 - pa_up
+
+	banco = pd.DataFrame()
+
+	i = 0
+	while i < 1.01:
+		pb_left = i
+		pb_right = abs(round(1 - pb_left,2))
+		coord = [-2,4,0,2]
+		result = (coord[0]*pa_up*pb_left) + (coord[1]*pa_down*pb_left) + (coord[2]*pa_up*pb_right) + (coord[3]*pa_down*pb_right)
+		banco = banco.append([[pa_up,pa_down,pb_left,pb_right,result]])
+		i = i + 0.01
+		
+	banco.columns = ['pa_up','pa_down','pb_left','pb_right','result']
+
+	if banco[banco['result'] == max(banco['result'])]['pb_left'].shape[0] != 1:
+		return max(banco[banco['result'] == max(banco['result'])]['pb_left'])
+	else:
+		return max(banco[banco['result'] == max(banco['result'])]['pb_left'])
+
+
+# retorno das estrategias
+
+#####################################################################################################
+# Gerando o gráfico - fonte: https://www.geeksforgeeks.org/contour-plot-using-matplotlib-python/
+#####################################################################################################
+
+fig = plt.figure(dpi=120)
+ax = plt.axes()
+ax.grid(color='gray',linewidth=.2)
+
+max_val = 100
+q1 = np.linspace(0,1,max_val)
+q2 = np.linspace(0,1,max_val)
+  
+ax.set_title('cap 30.5 - Coexistência')
+ax.set_xlabel('Linha - Prob Lutar')
+ax.set_ylabel('Coluna - Prob Lutar')
+
+# lines
+resultado_a = list()
+resultado_b = list()
+for x in range(0,len(q2)):
+	print(x/len(q2))
+	resultado_a.append(melhor_a(q2[x]))
+	resultado_b.append(melhor_b(q1[x]))
+
+plt.plot(resultado_a, q2,'-',color='blue', label='Melhor Resp Linha',linewidth=4)
+plt.plot(q1, resultado_b,'-',color='red', label='Melhor Resp Coluna',linewidth=4)
+
+plt.xlim(0,1)
+plt.ylim(0,1)
+plt.legend(loc='upper left')
+
+plt.show()
+
+# Grafico 2
+fig = plt.figure(dpi=120)
+ax = plt.axes()
+ax.grid(color='gray',linewidth=.2)
+
+max_val = 100
+q1 = np.linspace(0,1,max_val)
+q2 = np.linspace(0,1,max_val)
+  
+ax.set_title('cap 30.5 - Coexistência')
+ax.set_xlabel('proporção de lutadores')
+ax.set_ylabel('retorno')
+
+plt.plot(q1,(-2*q1+4*(1-q1)),'-',color='blue', label='Retorno Lutadores',linewidth=2)
+plt.plot(q1, (2*(1-q1)),'-',color='red', label='Retorno Cooperadores',linewidth=2)
+
+plt.xlim(0)
+plt.ylim(0)
+plt.legend(loc='upper left')
+
+plt.show()
