ea9383b7daf356ad305dbfcb9c0e160124665276
Bruno Ruas <bruno.ruas2@gmail.com>
2022-01-07 18:57:26 -0400

    cap 32.3 (done)

--- Microeconomia/Microeconomics - Hal Varian/models/scripts/varian_31.1_caixa_edgeworth.py
+++ Microeconomia/Microeconomics - Hal Varian/models/scripts/varian_31.1_caixa_edgeworth.py
@@ -1,5 +1,6 @@
 # Modulos usados
 import os
+from matplotlib import colors
 import numpy as np
 import pandas as pd
 import matplotlib as mpl
@@ -28,32 +29,31 @@ def fu_b(x1,x2,a,b):
 #####################################################################################################
 # Gerando o gráfico - fonte: https://pythontic.com/image-processing/pillow/blend
 #####################################################################################################
-max_val = 100
-q1 = np.linspace(0,max_val,100)
-q2 = np.linspace(0,max_val,100)
 
 # graph
-
 fig = plt.figure(dpi=120)
 ax = host_subplot(111)
 ax.grid(color='gray',linewidth=.2)
 
-ax.set_title('cap 32.1 - Caixa de Edgeworth\n')
+ax.set_title('cap 32.3 - Caixa de Edgeworth\n')
 ax.set_xlabel('Bem 1')
 ax.set_ylabel('Bem 2')
 
 # Creating 2-D grid of features
-[X, Y] = np.meshgrid(q1, q2)
-  
+max_val = 100
+q1 = np.linspace(0,max_val,100)
+q2 = np.linspace(0,max_val,100)
+[X, Y] = np.meshgrid(q1, q2)  
+
 # plots contour lines
 Z1 = fu_a(X,Y,0.5,0.5)
-CL1 = ax.contour(X, Y, Z1, colors="red", levels = range(10,1000,10)) # , cmap="winter"
+CL1 = ax.contour(X, Y, Z1, colors="red", levels = range(10,100,20), alpha = 1) # , cmap="winter"
 ax.clabel(CL1, inline=1, fontsize=10)
 
 ax.yaxis.tick_left()
 ax.yaxis.set_ticks_position('both')
 ax.xaxis.tick_bottom()
-ax.xaxis.set_ticks_position('both') # THIS IS THE ONLY CHANGE
+ax.xaxis.set_ticks_position('both')
 
 plt.xlim(0,100)
 plt.ylim(0,100)
@@ -62,29 +62,54 @@ ax = host_subplot(111)
 ax.grid(color='gray',linewidth=.2)
 
 Z2 = fu_b(X,Y,0.5,0.5)
-CL2 = ax.contour(X, Y, Z2, colors="cyan", levels = range(10,1000,10)) # , cmap="winter"
+CL2 = ax.contour(X, Y, Z2, colors="cyan", levels = range(10,100,20), alpha = 1) # , cmap="winter"
 ax.clabel(CL2, inline=1, fontsize=10)
 
+# adding line graphs
+plt.plot(q1,q2,color = 'white')
+
+# adding points with a label
+coord = [29,87] # aqui os eixos estao invertidos Y e X
+plt.plot(coord[0],coord[1],marker='o',color='white')
+plt.annotate('  Dotação\n  Inicial',(coord[0],coord[1]))
+
+coord = [50,50] # aqui os eixos estao invertidos Y e X
+plt.plot(coord[0],coord[1],marker='o',color='white')
+plt.annotate('  Alocação\n  Final',(coord[0],coord[1]))
+
+# putting arows
+'''
+coord = [0,40]
+ax.text(
+    coord[0], coord[1], "Bem 2 para consumidor B", ha="center", va="center", rotation=-90, size=10,
+    bbox=dict(boxstyle="rarrow,pad=0.3", fc="royalblue", ec="royalblue", lw=1))
+
+coord = [40,0]
+ax.text(
+    coord[0], coord[1], "Bem 1 para consumidor B", ha="center", va="center", rotation=0, size=10,
+    bbox=dict(boxstyle="larrow,pad=0.3", fc="royalblue", ec="royalblue", lw=1))
+
+coord = [100,60]
+ax.text(
+    coord[0], coord[1], "Bem 2 para consumidor A", ha="center", va="center", rotation=-90, size=10,
+    bbox=dict(boxstyle="larrow,pad=0.3", fc="darkred", ec="darkred", lw=1))
+
+coord = [60,100]
+ax.text(
+    coord[0], coord[1], "Bem 1 para consumidor A", ha="center", va="center", rotation=0, size=10,
+    bbox=dict(boxstyle="rarrow,pad=0.3", fc="darkred", ec="darkred", lw=1))
+'''
 ax.yaxis.tick_right()
 ax.yaxis.set_ticks_position('both')
 ax.xaxis.tick_top()
-ax.xaxis.set_ticks_position('both') # THIS IS THE ONLY CHANGE
+ax.xaxis.set_ticks_position('both')
 
 plt.xlim(100,0)
 plt.ylim(100,0)
 
 #plt.legend(loc='upper left')
-plt.savefig('img2.png', transparent=True)
+path = \
+r'C:\Users\bruno\Documents\Metadata\Meus_Estudos\Microeconomia'\
+r'\Microeconomics - Hal Varian\images\cap31_1-caixa_edgeworth.png'
 
-## merge images
-# Take two images for blending them together   
-image1 = Image.open("img1.png")
-image2 = Image.open("img2.png")
-
-# alpha-blend the images with varying values of alpha
-alphaBlended1 = Image.blend(image1, image2, alpha=1)
-
-# Display the alpha-blended images
-alphaBlended1.save(r'C:\Users\bruno\Documents\Metadata\Meus_Estudos\Microeconomia\Microeconomics - Hal Varian\images\cap31_1-caixa_edgeworth.png')
-
-os.remove('img2.png')
+plt.savefig(path, transparent=True)
