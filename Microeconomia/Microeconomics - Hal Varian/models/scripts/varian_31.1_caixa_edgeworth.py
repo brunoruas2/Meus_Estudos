e6d08ccb596069ef6a037a219cc6179c705cbda4
Bruno Ruas <bruno.ruas2@gmail.com>
2022-01-09 12:04:28 -0400

    cap 31.4 (doing)

--- Microeconomia/Microeconomics - Hal Varian/models/scripts/varian_31.1_caixa_edgeworth.py
+++ Microeconomia/Microeconomics - Hal Varian/models/scripts/varian_31.1_caixa_edgeworth.py
@@ -20,34 +20,55 @@ plt.style.use('dark_background')
 # Funções usadas no modelo
 #####################################################################################################
 # funcoes de utilidade
-def fu_a(x1,x2,a,b):
-	return (x1**a)*(x2**b)
+def fu_a(x1,x2,alpha,beta):
+	return (x1**alpha)*(x2**beta)
 
 def fu_b(x1,x2,a,b):
 	return (x1**a)*(x2**b)
 
+def dem(a,b,wa1,wa2,p1,p2):
+	return [(a*(wa1*p1 + wa2 * p2))/(p1),(b*(wa1*p1 + wa2 * p2))/(p2)]
+
+def dotacao(w1,w2,x1,p1,p2):
+	return ((w1*p1 + w2*p2) - p1*x1)/p2
+
 #####################################################################################################
 # Gerando o gráfico - fonte: https://pythontic.com/image-processing/pillow/blend
 #####################################################################################################
+## dados
+max_val = 100
+q1 = np.linspace(0,max_val,100)
+q2 = np.linspace(0,max_val,100)
+
+alpha = 0.1
+beta = 0.9
+a = 0.5
+b = 0.5
+
+p1 = 1
+p2 = 2
+
+x1a = dem(alpha,beta,80,20,p1,p2)[0]
+x2a = dem(alpha,beta,80,20,p1,p2)[1]
+x1b = dem(a,b,20,80,p1,p2)[0]
+x2b = dem(a,b,20,80,p1,p2)[1]
 
 # graph
 fig = plt.figure(dpi=120)
 ax = host_subplot(111)
 ax.grid(color='gray',linewidth=.2)
 
-ax.set_title('cap 32.3 - Caixa de Edgeworth\n')
+ax.set_title('cap 32.4 - Caixa de Edgeworth (p1 = 1 e p2 = 2)\n')
 ax.set_xlabel('Bem 1')
 ax.set_ylabel('Bem 2')
 
 # Creating 2-D grid of features
-max_val = 100
-q1 = np.linspace(0,max_val,100)
-q2 = np.linspace(0,max_val,100)
 [X, Y] = np.meshgrid(q1, q2)  
 
 # plots contour lines
-Z1 = fu_a(X,Y,0.5,0.5)
-CL1 = ax.contour(X, Y, Z1, colors="red", levels = range(10,100,20), alpha = 1) # , cmap="winter"
+Z1 = fu_a(X,Y,0.1,0.9)
+#CL1 = ax.contour(X, Y, Z1, colors="red", levels = range(0,100,10), alpha = 1) # , cmap="winter"
+CL1 = ax.contour(X, Y, Z1, colors="red", levels = [fu_a(x1a,x2a,alpha,beta)], alpha = 1) # , cmap="winter"
 ax.clabel(CL1, inline=1, fontsize=10)
 
 ax.yaxis.tick_left()
@@ -62,21 +83,23 @@ ax = host_subplot(111)
 ax.grid(color='gray',linewidth=.2)
 
 Z2 = fu_b(X,Y,0.5,0.5)
-CL2 = ax.contour(X, Y, Z2, colors="cyan", levels = range(10,100,20), alpha = 1) # , cmap="winter"
+#CL2 = ax.contour(X, Y, Z2, colors="cyan", levels = range(0,100,10), alpha = 1) # , cmap="winter"
+CL2 = ax.contour(X, Y, Z2, colors="cyan", levels = [fu_b(x1b,x2b,a,b)], alpha = 1) # , cmap="winter"
 ax.clabel(CL2, inline=1, fontsize=10)
 
 # adding line graphs
-plt.plot(q1,q2,color = 'white')
+#plt.plot(q1,q2,color = 'white') # curva de contrato
+plt.plot(q1,dotacao(20,80,q1,p1,p2),color = 'white') # curva de dotacao
 
 # adding points with a label
-coord = [29,87] # aqui os eixos estao invertidos Y e X
+coord = [20,80] # aqui os eixos estao invertidos Y e X
 plt.plot(coord[0],coord[1],marker='o',color='white')
 plt.annotate('  Dotação\n  Inicial',(coord[0],coord[1]))
-
+'''
 coord = [50,50] # aqui os eixos estao invertidos Y e X
 plt.plot(coord[0],coord[1],marker='o',color='white')
-plt.annotate('  Alocação\n  Final',(coord[0],coord[1]))
-
+plt.annotate('  Alocação\n  Final\n  Possível',(coord[0],coord[1]))
+'''
 # putting arows
 '''
 coord = [0,40]
@@ -110,6 +133,6 @@ plt.ylim(100,0)
 #plt.legend(loc='upper left')
 path = \
 r'C:\Users\bruno\Documents\Metadata\Meus_Estudos\Microeconomia'\
-r'\Microeconomics - Hal Varian\images\cap31_1-caixa_edgeworth.png'
+r'\Microeconomics - Hal Varian\images\cap31_1-caixa_edgeworth_9.png'
 
 plt.savefig(path, transparent=True)
