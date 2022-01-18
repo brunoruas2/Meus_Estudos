c66da3d0edd07f47dafb2d10035ca2bd9a335116
Bruno Ruas <bruno.ruas2@gmail.com>
2022-01-18 13:13:55 -0400

    cap 33.3 (doing)

--- Microeconomia/Microeconomics - Hal Varian/models/scripts/varian_33.1_robinson_crusoe.py
+++ Microeconomia/Microeconomics - Hal Varian/models/scripts/varian_33.1_robinson_crusoe.py
@@ -27,6 +27,9 @@ def fu_rob(x1,x2,alpha,beta):
 def f_prod(x1,a):
 	return x1**a
 
+def isolucro(pi,w,L):
+	return pi + w*L
+
 #####################################################################################################
 # Gerando o gráfico
 #####################################################################################################
@@ -43,25 +46,35 @@ fig = plt.figure(dpi=120)
 ax = host_subplot(111)
 ax.grid(color='gray',linewidth=.2)
 
-ax.set_title('cap 33 - A economia de Robinson Crusoé')
+ax.set_title('cap 33 - A maximização do Lucro')
 ax.set_xlabel('Trabalho = 100 - Lazer')
 ax.set_ylabel('Cocos')
 
-plt.plot(q1,f_prod(14*q1,1/1.8),color='red',label='função de produção')
+plt.plot(q1,f_prod(14*q1,1/2),color='red',label='função de produção')
 
-for i in [40,45,50]:
+'''
+# curvas de indiferença
+for i in [38]:
 	plt.plot(q1,(i/(100-q1)**0.5)**(1/0.5),label='C.I. = {}'.format(i))
+'''
 
-coord = [35.71,31.58]
+# curva de iso lucro
+for i in range(12,13,1):
+	plt.plot(q1,isolucro(i,0.3,q1),label='Isolucro = {}'.format(i))
+
+
+# point
+coord = [41.5,24.5]
 plt.plot(coord[0],coord[1],marker='o',color='white')
-plt.annotate('  Cesta',(coord[0],coord[1]))
+#plt.annotate('  Cesta',(coord[0],coord[1]))
+
 
 plt.xlim(0,100)
-plt.ylim(0,100)
+plt.ylim(0,50)
 
 plt.legend(loc='upper left')
 
-path = r'C:\Users\bruno\Documents\Metadata\Meus_Estudos\Microeconomia\Microeconomics - Hal Varian\images\cap33_1-economia_robinson.png'
+path = r'C:\Users\bruno\Documents\Metadata\Meus_Estudos\Microeconomia\Microeconomics - Hal Varian\images\cap33_3-max_lucro.png'
 
 plt.savefig(path, transparent=True)
 
