1a74d5895860f8a9b996258b55b59c6cc9f18cc7
Bruno Ruas <bruno.ruas2@gmail.com>
2022-01-19 08:16:09 -0400

    cap 33.4 (done)

--- Microeconomia/Microeconomics - Hal Varian/models/scripts/varian_33.1_robinson_crusoe.py
+++ Microeconomia/Microeconomics - Hal Varian/models/scripts/varian_33.1_robinson_crusoe.py
@@ -16,6 +16,7 @@ from mpl_toolkits.axes_grid1 import host_subplot
 pd.options.mode.chained_assignment = None  # default='warn'
 
 plt.style.use('dark_background')
+#plt.rcParams['text.usetex'] = True
 
 #####################################################################################################
 # Funções usadas no modelo
@@ -46,27 +47,25 @@ fig = plt.figure(dpi=120)
 ax = host_subplot(111)
 ax.grid(color='gray',linewidth=.2)
 
-ax.set_title('cap 33 - A maximização do Lucro')
-ax.set_xlabel('Trabalho = 100 - Lazer')
+ax.set_title('cap 33 - Equilíbrio no Consumo e Produção')
+ax.set_xlabel('<- Lazer | trabalho -> ')
 ax.set_ylabel('Cocos')
 
+# funcao de producao
 plt.plot(q1,f_prod(14*q1,1/2),color='red',label='função de produção')
 
-'''
 # curvas de indiferença
-for i in [38]:
-	plt.plot(q1,(i/(100-q1)**0.5)**(1/0.5),label='C.I. = {}'.format(i))
-'''
+plt.plot(q1,(38/(100-q1)**0.5)**(1/0.5),label='C.I.')
 
-# curva de iso lucro
-for i in range(12,13,1):
-	plt.plot(q1,isolucro(i,0.3,q1),label='Isolucro = {}'.format(i))
+# curva de iso lucro e reta orçamentaria
+#plt.plot(q1,isolucro(11.5,0.31,q1),label='Isolucro')
+plt.plot(q1,isolucro(11.5,0.31,q1),label='Restrição\nOrçamentária')
 
 
 # point
-coord = [41.5,24.5]
+coord = [33.3,22]
 plt.plot(coord[0],coord[1],marker='o',color='white')
-#plt.annotate('  Cesta',(coord[0],coord[1]))
+#plt.annotate(r'$\pi^*$',(coord[0],coord[1]+1),fontsize=20)
 
 
 plt.xlim(0,100)
@@ -74,7 +73,8 @@ plt.ylim(0,50)
 
 plt.legend(loc='upper left')
 
-path = r'C:\Users\bruno\Documents\Metadata\Meus_Estudos\Microeconomia\Microeconomics - Hal Varian\images\cap33_3-max_lucro.png'
+#plt.show()
 
+path = r'C:\Users\bruno\Documents\Metadata\Meus_Estudos\Microeconomia\Microeconomics - Hal Varian\images\cap33_5-equilibrio.png'
 plt.savefig(path, transparent=True)
 
