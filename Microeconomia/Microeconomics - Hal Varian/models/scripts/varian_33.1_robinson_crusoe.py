1d5be973d0513f4aa52f3548b662d105f6347e04
Bruno Ruas <bruno.ruas2@gmail.com>
2022-01-25 22:25:44 -0400

    cap 33 (done)

--- Microeconomia/Microeconomics - Hal Varian/models/scripts/varian_33.1_robinson_crusoe.py
+++ Microeconomia/Microeconomics - Hal Varian/models/scripts/varian_33.1_robinson_crusoe.py
@@ -69,24 +69,24 @@ ax.set_ylabel('Cocos')
 
 # curvas de indiferença
 #plt.plot(q1,(38/(100-q1)**0.5)**(1/0.5),label='C.I.')
-#for i in range(0,500,40):
-#	plt.plot(q1,((i/(q1**0.5))**2),color='blue',alpha=1)
-#	plt.plot(q1,(max_val - (i/(max_val - q1)**0.5)**2),color='yellow',alpha=1)
+for i in range(0,500,40):
+	plt.plot(q1,((i/(q1**0.5))**2),color='blue',alpha=1)
+	plt.plot(q1,(max_val - (i/(max_val - q1)**0.5)**2),color='yellow',alpha=1)
 
 # possibilidade de producao
 x = [0,50,145,200,245,287,300] # q1
 y = [300,287,245,200,145,50,0] # 10 * (10 - (q1/20)) + 20 * (10 - (q1/10))
-plt.plot(x,y,color='red',label='Fronteira de\nPossibilidade de\nProdução',linewidth=2)
-plt.fill_between(x, y, color='red',alpha=0.1) # area vermelha
+plt.plot(x,y,color='red',label='FPP',linewidth=2)
+plt.fill_between(x, y, color='red',alpha=0.08) # area vermelha
 
 # curva de iso lucro e reta orcamentaria
 #plt.plot(q1,isolucro(11.5,0.31,q1),label='Isolucro')
 #plt.plot(q1,isolucro(11.5,0.31,q1),label='Restrição\nOrçamentária')
-for pi in range(0,1000,98):
-	pi_L = pi + 10 # lucro mais trabalho-otimo
+for pi in range(392,393,392):
+	pi_L = pi + 10 # lucro + trabalho-otimo
 	pC = 5
 	pF = 5
-	plt.plot(q1,(pi_L-(pF/pC)*q1),color='blue')
+	plt.plot(q1,(pi_L-(pF/pC)*q1),color='white',label='Isolucro')
 
 # point
 #coord = [51,30]
@@ -96,14 +96,14 @@ for pi in range(0,1000,98):
 coord = [200,200]
 plt.plot(coord[0],coord[1],marker='o',color='white')
 #plt.annotate(r' TMS = TMT',(coord[0],coord[1]+1),fontsize=10)
-plt.annotate(r' TMT = Isolucro',(coord[0],coord[1]+1),fontsize=10)
+plt.annotate(r' TMT = Isolucro = TMS',(coord[0]-101,coord[1]-5),fontsize=10)
 
 plt.xlim(0,300)
 plt.ylim(0,300)
 
 plt.legend(loc='upper right')
 
-path = r'C:\Users\bruno\Documents\Metadata\Meus_Estudos\Microeconomia\Microeconomics - Hal Varian\images\cap33_12-max_lucros.png'
+path = r'C:\Users\bruno\Documents\Metadata\Meus_Estudos\Microeconomia\Microeconomics - Hal Varian\images\cap33_13-equilibrio_geral.png'
 plt.savefig(path, transparent=True)
 
 plt.show()
