7bda49e3a0de1bfdc4296c17b18162620942e002
Bruno Ruas <bruno.ruas2@gmail.com>
2022-03-13 15:57:12 -0400

    aula 07 (doing)

--- Microeconomia/_Projeto_Micro_Grad_Edition/micro_II/2022.01/create_pdf.bat
+++ Microeconomia/_Projeto_Micro_Grad_Edition/micro_II/2022.01/create_pdf.bat
@@ -1,1 +1,1 @@
-pdflatex C:\Users\bruno\Documents\Metadata\Meus_Estudos\Microeconomia\_Projeto_Micro_Grad_Edition\micro_II\2022.01\2022.01.tex
+pdflatex C:\Users\bruno\Documents\GitHub\Meus_Estudos\Microeconomia\_Projeto_Micro_Grad_Edition\micro_II\2022.01\2022.01.tex
